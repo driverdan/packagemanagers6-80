f175b45bc00a979c9caad09f751bf2ee0505b02b
Dan DeFelippi <dan@driverdan.com>
2019-04-29 20:21:51 +0000

    add package blueprint-django-image-gallery

--- packagemanagers6_80/urls.py
+++ packagemanagers6_80/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^api/v1/', include('home.api.v1.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^photos/', include('photologue.urls', namespace='photologue')),
 ]
 
 admin.site.site_header = 'packagemanagers6'
